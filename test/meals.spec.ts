951117bc62d9f473345411820615b53294307209
Felipe Martins <felipemjesus@outlook.com>
2024-10-11 23:20:56 -0300

    Add plus tests

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -201,4 +201,152 @@ describe('Meals', () => {
       .send()
       .expect(204)
   })
+
+  it('should be able to metrics count meals registered by a specific user', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'New user',
+        email: 'newuser@email.com',
+      })
+      .expect(201)
+
+    const userId = createUserResponse.body.user.id
+
+    await request(app.server)
+      .post('/meals')
+      .send({
+        title: 'New meal',
+        description: 'Description of new meal',
+        date: '2024-10-01',
+        hour: '12:00:00',
+        is_diet: 1,
+        user_id: userId,
+      })
+      .expect(201)
+
+    const getMealsResponse = await request(app.server)
+      .get(`/meals/${userId}/registered`)
+      .expect(200)
+
+    expect(getMealsResponse.body.user).toEqual(
+      expect.objectContaining({
+        count: 1,
+      }),
+    )
+  })
+
+  it('should be able to metrics count meals in diet by a specific user', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'New user',
+        email: 'newuser@email.com',
+      })
+      .expect(201)
+
+    const userId = createUserResponse.body.user.id
+
+    await request(app.server)
+      .post('/meals')
+      .send({
+        title: 'New meal',
+        description: 'Description of new meal',
+        date: '2024-10-01',
+        hour: '12:00:00',
+        is_diet: 1,
+        user_id: userId,
+      })
+      .expect(201)
+
+    const getMealsResponse = await request(app.server)
+      .get(`/meals/${userId}/in-diet`)
+      .expect(200)
+
+    expect(getMealsResponse.body.user).toEqual(
+      expect.objectContaining({
+        count: 1,
+      }),
+    )
+  })
+
+  it('should be able to metrics count meals out of diet by a specific user', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'New user',
+        email: 'newuser@email.com',
+      })
+      .expect(201)
+
+    const userId = createUserResponse.body.user.id
+
+    await request(app.server)
+      .post('/meals')
+      .send({
+        title: 'New meal',
+        description: 'Description of new meal',
+        date: '2024-10-01',
+        hour: '12:00:00',
+        is_diet: 0,
+        user_id: userId,
+      })
+      .expect(201)
+
+    const getMealsResponse = await request(app.server)
+      .get(`/meals/${userId}/out-diet`)
+      .expect(200)
+
+    expect(getMealsResponse.body.user).toEqual(
+      expect.objectContaining({
+        count: 1,
+      }),
+    )
+  })
+
+  it('should be able to metrics count meals sequence in diet by a specific user', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'New user',
+        email: 'newuser@email.com',
+      })
+      .expect(201)
+
+    const userId = createUserResponse.body.user.id
+
+    await request(app.server)
+      .post('/meals')
+      .send({
+        title: 'New meal',
+        description: 'Description of new meal',
+        date: '2024-10-01',
+        hour: '12:00:00',
+        is_diet: 1,
+        user_id: userId,
+      })
+      .expect(201)
+
+    await request(app.server)
+      .post('/meals')
+      .send({
+        title: 'New meal',
+        description: 'Description of new meal',
+        date: '2024-10-01',
+        hour: '12:00:00',
+        is_diet: 1,
+        user_id: userId,
+      })
+      .expect(201)
+
+    const getMealsResponse = await request(app.server)
+      .get(`/meals/${userId}/sequence-in-diet`)
+      .expect(200)
+
+    expect(getMealsResponse.body.user).toEqual(
+      expect.objectContaining({
+        count: 1,
+      }),
+    )
+  })
 })
